265bc870a25fb005fd39796f34b3ccd36ec7d9be
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-18 19:53:03 +0700

    Mengerjakan Tugas 24 Javascript

---
+++ tugas24.js
@@ -0,0 +1,6 @@
+let angka = "32 33 16 40 9 7 11 2 44 1 66";
+angka = angka.split(" ");
+
+console.log("Sebelumnya :", angka.toString());
+console.log("Acsending :", angka.sort().toString());
+console.log("Descending :", angka.sort().reverse().toString());
